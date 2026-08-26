fa7828912ebf9bd0fdcb5c7c7d359b3a907c5df5
MATIEYENDOU <matieyendou@fedora.home>
2026-08-26 19:47:58 +0200

    feat:Ajout des dependance firebase

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:44851/7yJeQCv-lJk=';
+const String kWidgetPreviewService =
+    'widget-preview-1592fc8d-5238-41af-893e-0edfeda280cd';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-1592fc8d-5238-41af-893e-0edfeda280cd';
+const String kProjectRootPath =
+    r'/home/matieyendou/D-CLIC/FORMATION-DCLIC-ACTIVITE-4';
--- lib/main.dart
+++ lib/main.dart
@@ -101,11 +101,11 @@ class PartieTitre extends StatelessWidget {
         child: Column(
           children: [
             //titre principal
-            Text(
+            const Text(
               'Bienvenue sur l\'application Magazine Infos !',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             //titre secondaire
             Text(
               'Découvrez les dernières actualités et articles intéressants.',
@@ -124,7 +124,7 @@ class PartieTexte extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.all(10),
-        child: Text(
+        child: const Text(
             'Dans un monde en constante évolution, notre objectif est de vous offrir un contenu riche, pertinent et accessible, qui met en lumière les tendances actuelles, les innovations, ainsi que les histoires qui façonnent notre quotidien.À travers ce magazine, nous abordons des thématiques variées allant de la technologie, à la culture, en passant par l’éducation, l’entrepreneuriat et les enjeux de société. Chaque article est conçu pour informer, éveiller la curiosité et apporter une nouvelle perspective sur le monde qui nous entoure.Ce numéro met particulièrement l’accent sur [thème du numéro : ex. l’innovation numérique / la jeunesse africaine / l’entrepreneuriat local], un sujet au cœur des transformations actuelles.'));
   }
 }
@@ -176,8 +176,8 @@ class PartieIcone extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
-              child: Column(
-                children: const [
+              child: const Column(
+                children: [
                   Icon(Icons.phone, color: Colors.brown),
                   SizedBox(height: 5),
                   Text('TEL', style: TextStyle(color: Colors.brown)),
@@ -185,8 +185,8 @@ class PartieIcone extends StatelessWidget {
               ),
             ),
             Container(
-              child: Column(
-                children: const [
+              child: const Column(
+                children: [
                   Icon(Icons.email, color: Colors.brown),
                   SizedBox(height: 5),
                   Text('MAIL', style: TextStyle(color: Colors.brown)),
@@ -194,8 +194,8 @@ class PartieIcone extends StatelessWidget {
               ),
             ),
             Container(
-              child: Column(
-                children: const [
+              child: const Column(
+                children: [
                   Icon(Icons.share, color: Colors.brown),
                   SizedBox(height: 5),
                   Text('PARTAGE', style: TextStyle(color: Colors.brown)),
